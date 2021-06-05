5b36f6be58674e0d988f276c342c2233a2d80eb1
Skyxim <70276018+Skyxim@users.noreply.github.com>
2021-06-05 17:43:40 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,10 +2,5 @@ console.log("start")
 let app = JSON.parse($request.body);
 app.storefrontId = '143380-1,29';
 console.log("wait...")
-console.log(app)
-sleep(1000).then(()=>{
-console.log("done")
-  
 $done({body:JSON.stringify(app)});
-})
 console.log("end")
